e27f123a14abc0c7ed0c62c177050775c78fb023
Claudio Schaad <schaad@pog.ch>
2019-07-07 19:24:23 +0200

    Fixed startup file

--- Schaad.Accounting/start.cmd
+++
@@ -1,6 +0,0 @@
-cd D:\Documents\SchaadAccounting\csharp
-git fetch && git checkout master
-git pull
-cd Schaad.CSharp\Schaad.Accounting
-dotnet restore
-dotnet run
---
+++ start.cmd
@@ -0,0 +1,6 @@
+cd C:\Users\schaa\Accounting
+git fetch && git checkout master
+git pull
+cd Schaad.Accounting
+dotnet restore
+dotnet run

0ec68b8e4160eb20d0e3ffe8c7ea66d184e7a1aa
Claudio Schaad <c.schaad@pog.ch>
2025-04-26 11:27:44 +0200

    Remove fx service and use 1:1 for USD and EUR

--- Schaad.Accounting/Controllers/TransactionController.cs
+++ Schaad.Accounting/Controllers/TransactionController.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Schaad.Accounting.Interfaces;
 using Schaad.Accounting.Models;
-using Schaad.Finance.Api;
 
 namespace Schaad.Accounting.Controllers
 {
@@ -12,15 +11,13 @@ namespace Schaad.Accounting.Controllers
         private readonly IAccountRepository accountRepository;
         private readonly ITransactionRepository transactionRepository;
         private readonly IViewService viewService;
-        private readonly IFxService fxService;
         private readonly ISettingsService settingsService;
 
-        public TransactionController(IViewService viewService, IAccountRepository accountRepository, ITransactionRepository transactionRepository, IFxService fxService, ISettingsService settingsService)
+        public TransactionController(IViewService viewService, IAccountRepository accountRepository, ITransactionRepository transactionRepository, ISettingsService settingsService)
         {
             this.viewService = viewService;
             this.transactionRepository = transactionRepository;
             this.accountRepository = accountRepository;
-            this.fxService = fxService;
             this.settingsService = settingsService;
         }
 
@@ -56,16 +53,6 @@ namespace Schaad.Accounting.Controllers
         {
             transactionRepository.SaveTransaction(transaction);
             return new JsonResult("");
-        }
-
-        public string GetFxRate(string originAccountId, string targetAccountId, DateTime date)
-        {
-            var settings = settingsService.GetSettings();
-            var originAccount = accountRepository.GetAccount(originAccountId);
-            var targetAccount = accountRepository.GetAccount(targetAccountId);
-            var targetAccountCurrency = string.IsNullOrEmpty(targetAccount.Currency) ? "CHF" : targetAccount.Currency;
-            var fxRate = fxService.GetFxRate(originAccount.Currency, targetAccountCurrency, date, settings.FixerIoApiKey);
-            return fxRate.ToString();
         }
     }
 }
